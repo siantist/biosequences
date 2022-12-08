c3948d735c18595862cd67cf9a73c1d5aa58117e
siantist <104287799+siantist@users.noreply.github.com>
2022-12-07 19:02:43 -0500

    Create procedure1.py

---
+++ word_rep_graphs/procedure1.py
@@ -0,0 +1,9 @@
+# https://www.math.ru.nl/~bosma/Students/BasBroereMSc.pdf
+# word w represents G iff for every edge (x,y) the letters x,y alternate in w 
+
+# preprocess dna sequence by defining new "words" from groups of letters
+
+# put dna sequence into graph 
+dna_graph = word2graph(dna_seq, n)
+
+# reveal quality of 
